6fb08a610ecacb435e371c671858082af80c66d3
live-beta <wanjala.sam.s@gmail.com>
2018-11-08 19:35:25 +0300

    [add] data object - Implement a plain old java object for retrofit - Import retrofit library

---
+++ app/src/androidTest/java/com/nanodegree/sam/baker/models/Ingredient.java
@@ -0,0 +1,35 @@
+package com.nanodegree.sam.baker.models;
+
+public class IngredientModel {
+
+
+    private Integer quantity;
+    private String measure;
+    private String ingredient;
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getMeasure() {
+        return measure;
+    }
+
+    public void setMeasure(String measure) {
+        this.measure = measure;
+    }
+
+    public String getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(String ingredient) {
+        this.ingredient = ingredient;
+    }
+
+
+}
--- app/src/main/java/com/nanodegree/sam/baker/activities/MainActivity.java
+++ app/src/main/java/com/nanodegree/sam/baker/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.nanodegree.sam.baker;
+package com.nanodegree.sam.baker.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.nanodegree.sam.baker.R;
 
 public class MainActivity extends AppCompatActivity {
 
