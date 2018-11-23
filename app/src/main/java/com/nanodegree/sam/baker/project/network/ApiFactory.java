55ccd80728b088404b191ea1b9e7c5924dbfd0bb
live-beta <wanjala.sam.s@gmail.com>
2018-11-23 21:06:52 +0300

    [update]: implement idling state
    - improve code testability

--- app/src/main/java/com/nanodegree/sam/baker/project/network/ApiFactory.java
+++ app/src/main/java/com/nanodegree/sam/baker/project/network/ApiFactory.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.nanodegree.sam.baker.R;
+import com.nanodegree.sam.baker.project.activities.IdlingResources;
 
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
@@ -21,12 +22,15 @@ public class ApiFactory {
         OkHttpClient.Builder builder = new OkHttpClient.Builder();
         OkHttpClient client = builder.build();
 
+        IdlingResources.registerOkHttp(client);
+
         Retrofit.Builder retrofit = new Retrofit.Builder()
                 .baseUrl(context.getString(R.string.base_url))
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .client(client);
 
         Retrofit retrofitInstance = retrofit.build();
+
         return retrofitInstance.create(FoodNetworkService.class);
 
     }
