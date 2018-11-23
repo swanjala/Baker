1831a0746acd48ea97a5f65cd79702ed89fe6f2c
live-beta <wanjala.sam.s@gmail.com>
2018-11-23 20:56:33 +0300

    [add] : add ideling resource registry

--- app/src/main/java/com/nanodegree/sam/baker/project/activities/IdlingResources.java
+++ app/src/main/java/com/nanodegree/sam/baker/project/activities/IdlingResources.java
@@ -1,4 +1,17 @@
 package com.nanodegree.sam.baker.project.activities;
-public class IdlingResources {
+
+import android.support.test.espresso.IdlingRegistry;
+
+import com.jakewharton.espresso.OkHttp3IdlingResource;
+
+import okhttp3.OkHttpClient;
+
+public abstract class IdlingResources {
+
+    public static void registerOkHttp(OkHttpClient okHttpClient) {
+        IdlingRegistry.getInstance().register(OkHttp3IdlingResource.create(
+                "okhttp", okHttpClient));
+    }
 
 }
+
